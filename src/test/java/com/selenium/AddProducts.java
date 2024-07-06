955e300eee7120b7d3b339a93b5f0e3932ee13cb
Icm <161704540+Hristo26@users.noreply.github.com>
2024-07-06 11:04:18 +0300

    Project1

--- src/test/java/com/selenium/AddProducts.java
+++ src/test/java/com/selenium/AddProducts.java
@@ -10,7 +10,8 @@ public class AddProducts extends TestUtil {
     public void AddItemToTheCart(){
        LoginPage loginPage = new LoginPage(driver);
        ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
-        productsPage.addToCartByProductName("backpack");
+       productsPage.addToCartByProductName("backpack");
+
 
         Assert.assertEquals(productsPage.getNumbersInTheCart(), 1, "Because we have one item in the cart.");
     }
